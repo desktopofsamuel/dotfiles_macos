bc05bdff1691d322dfdc1ce658baf602b7723894
Samuel W <35249407+desktopofsamuel@users.noreply.github.com>
2021-02-23 00:56:19 +0800

    Added duti in shell script & new extensions

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -5,3 +5,6 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle --verbose --file brew/Brewfile
+
+echo "\n<<< Default File Extensions Setup >>>\n"
+duti duti/duti
